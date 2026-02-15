c53871dd29efd2a69e027f0a87efca00d6eaefe7
Chris Carl <chrisbcarl@outlook.com>
2026-02-15 01:00:21 -0800

    core.data_structures.node - initial commit

---
+++ src/chriscarl/core/data_structures/node.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author:         Chris Carl
+Email:          chrisbcarl@outlook.com
+Date:           2026-02-15
+Description:
+
+core.data_structures.node is an overkill superclass node that can be used by lists, trees, graphs, etc.
+core are modules that define the bedrock from which other things do import. non-self-referential, low-import, etc.
+
+Updates:
+    2026-02-15 - core.data_structures.node - initial commit
+'''
+
+# stdlib imports
+from __future__ import absolute_import, print_function, division, with_statement  # , unicode_literals
+import os
+import sys
+import logging
+from typing import Optional, Generator, Tuple
+
+# third party imports
+
+# project imports
+
+SCRIPT_RELPATH = 'chriscarl/core/data_structures/node.py'
+if not hasattr(sys, '_MEIPASS'):
+    SCRIPT_FILEPATH = os.path.abspath(__file__)
+else:
+    SCRIPT_FILEPATH = os.path.abspath(os.path.join(sys._MEIPASS, SCRIPT_RELPATH))  # pylint: disable=no-member
+SCRIPT_DIRPATH = os.path.dirname(SCRIPT_FILEPATH)
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+THIS_MODULE = sys.modules[__name__]
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+
+# pretty print a binary tree
+import math
+import random
+from typing import List, Any
+
+
+class Node(object):
+    neighbors = []  # type: List[Optional['Node']]
+    data = None  # type: Any
+
+    def __init__(self, data):
+        self.neighbors = []
+        self.data = data
+
+    def __str__(self):
+        return f'Node[{self.data}]'
+
+    def __repr__(self):
+        return f'Node[{self.data}, {len(self.neighbors)}E]'
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return self.data == other.data
+
+    def __lt__(self, other):
+        if not isinstance(other, Node):
+            return False
+        if self.data is None:
+            if other.data is None:
+                return False
+            else:
+                return True
+        return self.data < other.data
+
+    def add_edge(self, node):
+        # type: (Node|Any) -> Node
+        if not isinstance(node, Node):
+            node = Node(node)
+        self.neighbors.append(node)
+        return node
+
+    @property
+    def edges(self):
+        return [n for n in self.neighbors]
---
+++ tests/core/data_structures/test_node.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author:         Chris Carl
+Email:          chrisbcarl@outlook.com
+Date:           2026-02-15
+Description:
+
+chriscarl.core.data_structures.node unit test.
+
+Updates:
+    2026-02-15 - tests.chriscarl.core.data_structures.node - initial commit
+'''
+
+# stdlib imports (expected to work)
+from __future__ import absolute_import, print_function, division, with_statement  # , unicode_literals
+import os
+import sys
+import logging
+import unittest
+
+# third party imports
+
+# project imports (expected to work)
+from chriscarl.core import constants
+from chriscarl.core.lib.stdlib.os import abspath
+from chriscarl.core.lib.stdlib.unittest import UnitTest
+
+# test imports
+import chriscarl.core.data_structures.node as lib
+
+SCRIPT_RELPATH = 'tests/core/data_structures/test_node.py'
+if not hasattr(sys, '_MEIPASS'):
+    SCRIPT_FILEPATH = os.path.abspath(__file__)
+else:
+    SCRIPT_FILEPATH = os.path.abspath(os.path.join(sys._MEIPASS, SCRIPT_RELPATH))  # pylint: disable=no-member
+SCRIPT_DIRPATH = os.path.dirname(SCRIPT_FILEPATH)
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+THIS_MODULE = sys.modules[__name__]
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+
+constants.fix_constants(lib)  # deal with namespace sharding the files across directories
+
+
+class TestCase(UnitTest):
+
+    def setUp(self):
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+
+    # @unittest.skip('lorem ipsum')
+    def test_case_0(self):
+        n0 = lib.Node(0)
+        n1 = lib.Node(1)
+        n0.add_edge(n1)
+        n0.add_edge(n1)
+        variables = [
+            (lambda x, y: x == y, (n0, n0)),
+            (lambda x, y: x == y, (n0, n1)),
+            (lambda x, y: x < y, (n0, n0)),
+            (lambda x, y: x < y, (n0, n1)),
+            (str, n0),
+            (repr, n0),
+        ]
+        controls = [
+            True,
+            False,
+            False,
+            True,
+            'Node[0]',
+            'Node[0, 2E]',
+        ]
+        self.assert_null_hypothesis(variables, controls)
+
+
+if __name__ == '__main__':
+    tc = TestCase()
+    tc.setUp()
+
+    try:
+        tc.test_case_0()
+    finally:
+        tc.tearDown()
