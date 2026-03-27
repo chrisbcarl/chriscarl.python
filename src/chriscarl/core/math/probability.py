f387be7f59db4f7ad66d372df6733f899ec81caf
Chris Carl <chrisbcarl@outlook.com>
2026-03-26 20:08:50 -0700

    probability, binomial_cdf

---
+++ src/chriscarl/core/math/probability.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r'''
+Author:         Chris Carl
+Email:          chrisbcarl@outlook.com
+Date:           2026-03-26
+Description:
+
+core.math.probability is all of the probability learnings I have had over time.
+core are modules that define the bedrock from which other things do import. non-self-referential, low-import, etc.
+
+Updates:
+    2026-03-26 - core.math.probability - initial commit
+'''
+
+# stdlib imports
+from __future__ import absolute_import, print_function, division, with_statement  # , unicode_literals
+import os
+import sys
+import logging
+
+# third party imports
+
+# project imports
+from chriscarl.core.math.counting import combination
+
+SCRIPT_RELPATH = 'chriscarl/core/math/probability.py'
+if not hasattr(sys, '_MEIPASS'):
+    SCRIPT_FILEPATH = os.path.abspath(__file__)
+else:
+    SCRIPT_FILEPATH = os.path.abspath(os.path.join(sys._MEIPASS, SCRIPT_RELPATH))  # pylint: disable=no-member
+SCRIPT_DIRPATH = os.path.dirname(SCRIPT_FILEPATH)
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+THIS_MODULE = sys.modules[__name__]
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+
+
+def binomial_cdf(l, n, p, precision=4):
+    # type: (int, int, float, int) -> float
+    r'''
+    Description:
+        binomial_exact_test, binomial_cdf
+        $X \overset{iid}{\sim} Bin(n, p), P(X \le l) = \displaystyle\sum_{k=0}^{l}p^{k}(1-p)^{n-k}$
+        This random variable realizes a 1 at some rate $p$ and realizes a 0 at some rate $1 - p$
+        Say you will experiment and realize/observe $n$ of these random variables.
+        What is the probability you will observe less than $m$ successes?
+    Example:
+        - Chance of heads 0.50, 10 coin flips, probability of observing LESS THAN 5 heads?
+            $X \overset{iid}{\sim} Bin(n=10, p=0.5)$
+            $P(X < 5) = P(X \le 4) = F(4) = 0.0751$
+            >>> binomial_cdf(4, 10, 0.5)
+        - Chance of failure 0.28, 1000 widgets, probability of observing MORE THAN 300 failures?
+            $X \overset{iid}{\sim} Bin(n=1000, p=0.28)$
+            $P(X > 300) = 1 - P(X \le 300) = 1 - F(300) = 0.0751$
+            >>> 1 - binomial_cdf(300, 1000, 0.28)
+    Arguments:
+        l: int
+            less than or equal to l "successes"
+        n: int
+            total population size
+        p: float
+            proportion of "successes"
+        precision: int
+            round to?
+    Returns:
+        float - probability
+    '''
+    if not (0 <= p and p <= 1.0):
+        raise ValueError(rf'required: $0 \le p \le 1$, provided p={p}')
+    probability = sum(combination(n, k) * (p**k) * ((1 - p)**(n - k)) for k in range(0, l + 1))
+    return round(probability, precision)
+
+
+binomial_exact_test = binomial_cdf
---
+++ tests/chriscarl/core/math/test_probability.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r'''
+Author:         Chris Carl
+Email:          chrisbcarl@outlook.com
+Date:           2026-03-26
+Description:
+
+chriscarl.core.math.probability unit test.
+
+pytest --cov=chriscarl.core.math.probability tests/chriscarl/core/math/test_probability.py --cov-report term-missing
+
+Updates:
+    2026-03-26 - tests.chriscarl.core.math.probability - initial commit
+'''
+
+# stdlib imports (expected to work)
+from __future__ import absolute_import, print_function, division, with_statement  # , unicode_literals
+import os
+import sys
+import logging
+import unittest
+from typing import Any
+
+# third party imports
+
+# project imports (expected to work)
+from chriscarl.core import constants
+from chriscarl.core.lib.stdlib.os import abspath
+from chriscarl.core.lib.stdlib.unittest import UnitTest
+
+# test imports
+import chriscarl.core.math.probability as lib
+
+SCRIPT_RELPATH = 'tests/chriscarl/core/math/test_probability.py'
+if not hasattr(sys, '_MEIPASS'):
+    SCRIPT_FILEPATH = os.path.abspath(__file__)
+else:
+    SCRIPT_FILEPATH = os.path.abspath(os.path.join(sys._MEIPASS, SCRIPT_RELPATH))  # pylint: disable=no-member
+SCRIPT_DIRPATH = os.path.dirname(SCRIPT_FILEPATH)
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+THIS_MODULE = sys.modules[__name__]
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+
+constants.fix_constants(lib)  # deal with namespace sharding the files across directories
+
+
+class TestCase(UnitTest):
+
+    def setUp(self):
+        super().setUp()
+        # do other set ups (set up super first)
+
+    def tearDown(self):
+        # do other tear downs (tear down super after)
+        super().tearDown()
+
+    # @unittest.skip('lorem ipsum')
+    def test_case_0(self):
+        variables = [
+            (lib.binomial_cdf, (4, 10, 0.5)),
+            (lib.binomial_exact_test, (300, 1000, 0.28)),
+        ]
+        controls = [
+            0.3770,  # phrased as P(X < 5) heads
+            1 - 0.0751,  # phrased as P(X > 300) failures
+        ]
+        self.assert_null_hypothesis(variables, controls)
+
+
+if __name__ == '__main__':
+    tc = TestCase()
+    tc.setUp()
+
+    try:
+        tc.test_case_0()
+    finally:
+        tc.tearDown()
